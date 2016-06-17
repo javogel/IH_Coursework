4ebe2a296967888b3b25be05e77b4ff9594ecc0b
Johannus V <johannus.vogel@gmail.com>
2016-06-17 15:03:39 +0200

    Improvements to chess game

--- Week_1/Day_4/Chess/Chess.rb
+++ Week_1/Day_4/Chess/Chess.rb
@@ -25,12 +25,47 @@ class ChessValidator
         if spot == "--"
           @board[y][x]=nil
         else
+          # uncomment below line if you want to make each a spot symbol
           @board[y][x] = spot.to_sym
+
+
         end
       end
     end
 
-    # print @board
+    # call functions that converts board to objects
+    convert_board_to_objects
+  end
+
+  def convert_board_to_objects
+    @board.each_with_index do |line, y|
+      line.each_with_index do |spot, x|
+      split_spot = spot.to_s.split("")
+      color = split_spot[0]
+
+        if spot == nil
+          next
+        elsif split_spot[1].upcase == "K"
+          @board[y][x]= King.new(color)
+        elsif split_spot[1].upcase == "Q"
+          @board[y][x]= Queen.new(color)
+        elsif split_spot[1].upcase == "R"
+          @board[y][x]= Rook.new(color)
+        elsif split_spot[1].upcase == "P"
+          @board[y][x]= Pawn.new(color)
+        elsif split_spot[1].upcase == "B"
+          @board[y][x]= Bishop.new(color)
+        elsif split_spot[1].upcase == "N"
+          @board[y][x]= Knight.new(color)
+
+        else
+
+         puts "Element missed"
+
+        end
+      end
+    end
+
   end
 
 
@@ -58,12 +93,23 @@ class ChessValidator
       from_converted = [ horizontal[from_letter.to_sym], vertical[from_number]]
       to_converted = [ horizontal[to_letter.to_sym], vertical[to_number]]
 
-      # validate_move(from_converted, to_converted)
+
 
       puts "form #{from_converted.to_s} going to  #{to_converted.to_s}"
+
+      validate_move(from_converted, to_converted)
   end
 
-  def validate_move
+
+  def validate_move(from, to)
+
+      piece_to_move = @board[from[0]][from[1]]
+
+      if @board[to[0]][to[1]] != nil
+        piece_to_move.valid_piece_move(from, to, @board)
+      else
+        puts "Invalid - Spot Taken"
+      end
 
   end
 
@@ -71,28 +117,93 @@ end
 
 
 class Piece
-  def initialize
+ attr_accessor :color
+
+  def initialize(color)
+    @color = color
   end
+
+
+
+  def is_spot_empty(to, board)
+    if board[to[0]][to[1]] != nil
+      true
+    else
+      false
+    end
+  end
+
+  def passes_move_restrictions(from, to, board)
+    return true
+  end
+
+
+  def valid_piece_move(from, to, board)
+    if is_spot_empty(to, board) && passes_move_restrictions(from, to, board)
+       puts "Valid"
+    else
+      # puts "Invalid"
+    end
+
+  end
+
+
+
+end
+
+class King < Piece
+  attr_accessor :color
+
+  def initialize(color)
+    super(color)
+  end
+
+
 end
 
 
-class Rook < Piece
-  def initialize
-  end
+class Queen < Piece
+  attr_accessor :color
 
+  def initialize(color)
+    super(color)
+  end
+end
+
+class Rook < Piece
+  attr_accessor :color
+
+  def initialize(color)
+    super(color)
+  end
+end
+
+class Knight < Piece
+  attr_accessor :color
+
+  def initialize(color)
+    super(color)
+  end
 end
 
 class Bishop < Piece
-  def initialize
+  attr_accessor :color
+
+  def initialize(color)
+    super(color)
   end
 end
 
 class Pawn < Piece
-  def initialize
+  attr_accessor :color
+
+  def initialize(color)
+    super(color)
   end
 end
 
 
 play = ChessValidator.new
 play.load_board
+# play.convert_board_to_objects
 play.load_moves
